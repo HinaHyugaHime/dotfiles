09c64bdf0bdc23ba8f07a12ed9c7ebb05d49c8d0
Hina <102651522+HinaHyugaHime@users.noreply.github.com>
2024-03-20 21:28:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-yay -S --needed waybar hyprland xdg-desktop-portal-{gtk,hyprland} udiskie network-manager-applet copyq swww nwg-{dock-hyprland,look,drawer,bar} polkit-kde-agent swayosd-git xwaylandvideobridge-git kitty swaync jq nerd-fonts sassc swappy hyprshot
+yay -S --needed waybar hyprland xdg-desktop-portal-{gtk,hyprland} udiskie network-manager-applet copyq swww nwg-{dock-hyprland,look,drawer,bar} polkit-kde-agent swayosd-git xwaylandvideobridge-git kitty swaync jq nerd-fonts sassc swappy hyprshot rofi
 
 wget "https://drive.usercontent.google.com/download?id=1wOWDBuUxu7xKlECDU9IORaDBpMb8pJ_Y&export=download&authuser=0&confirm=t&uuid=fad25827-99c2-4117-b5d5-f8878e4fe05e&at=APZUnTXwthyNPaqtll1dUqzu1xVS%3A1710987483480" -O config.tar.gz
 
