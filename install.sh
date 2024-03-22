d4b077aa99852d934037ab4c8a4e64e1e19c0b76
Hina <102651522+HinaHyugaHime@users.noreply.github.com>
2024-03-21 23:00:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-yay -S --needed waybar hyprland xdg-desktop-portal-{gtk,hyprland} udiskie network-manager-applet copyq swww nwg-{dock-hyprland,look,drawer,bar} polkit-kde-agent swayosd-git xwaylandvideobridge-git kitty swaync jq nerd-fonts sassc swappy hyprshot wofi
+yay -S --needed waybar hyprland xdg-desktop-portal-{gtk,hyprland} udiskie network-manager-applet copyq hyprpaper nwg-{dock-hyprland,look,drawer,bar} polkit-kde-agent swayosd-git xwaylandvideobridge-git kitty swaync jq nerd-fonts sassc swappy hyprshot wofi
 
 cd $HOME/.config/nwg-bar
 
