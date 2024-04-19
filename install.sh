8baa2fc0e80a70a6036827ae3cacd7f7b7844bda
Hina <102651522+HinaHyugaHime@users.noreply.github.com>
2024-04-19 17:27:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 yay -S --needed waybar hyprland eww ripgrep rofi grimshot thunar xdg-desktop-portal-{gtk,hyprland} udiskie network-manager-applet copyq swww thunar foot discord steam nwg-bar polkit-kde-agent swayosd-git xwaylandvideobridge-git kitty swaync jq nerd-fonts sassc swappy swaylock swayidle nwg-launchers nwg-bar nwg-drawer nwg-look hyprshot wofi
 
+sudo mv $HOME/dotfiles/.config $HOME/.config
+
 cd $HOME/.config/nwg-bar
 
 sed -i "s/riceuser/$USER/g" bar-screenshot.json
